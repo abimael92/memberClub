8c122a5373be1190e4a61c203481ee1849b20fd8
Abimael Garcia <Abimael.Garcia@improving.com>
2023-09-07 15:18:41 -0600

    change code for dashboard

--- client/src/components/layout/title/index.tsx
+++ client/src/components/layout/title/index.tsx
@@ -6,19 +6,19 @@ import logo from '../../../assets/logo.svg';
 import yariga from '../../../assets/pokemonClub.svg';
 
 export const Title: React.FC<TitleProps> = ({ collapsed }) => {
-	const { Link } = useRouterContext();
+    const { Link } = useRouterContext();
 
-	return (
-		<div style={{ marginTop: '40px' }}>
-			<Button fullWidth variant='text' disableRipple>
-				<Link to='/' sx={{ margin: '4px' }}>
-					{collapsed ? (
-						<img src={logo} alt='Yariga' width='28px' />
-					) : (
-						<img src={yariga} alt='pokemonClub' width='100px' />
-					)}
-				</Link>
-			</Button>
-		</div>
-	);
+    return (
+        <div style={{ marginTop: '40px' }}>
+            <Button fullWidth variant="text" disableRipple>
+                <Link to="/" sx={{ margin: '4px' }}>
+                    {collapsed ? (
+                        <img src={logo} alt="Yariga" width="28px" />
+                    ) : (
+                        <img src={yariga} alt="pokemonClub" width="100px" />
+                    )}
+                </Link>
+            </Button>
+        </div>
+    );
 };
